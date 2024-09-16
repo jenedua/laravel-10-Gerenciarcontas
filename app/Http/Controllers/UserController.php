729266fdf834e17cc25244e55a33aa8a9d189099
Fedner Dabady <jenasha29@gmail.com>
2024-09-16 12:14:28 -0300

    alterar user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
 {
